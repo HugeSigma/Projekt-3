5f9baabffcc0bee6e6d18025745fa0fe49263bba
Wilk <s197653@student.pg.edu.pl>
2024-04-18 22:21:47 +0200

    cmake dodany

---
+++ CMakeProjekt3/CMakeProjekt3.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProjekt3.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProjekt3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
