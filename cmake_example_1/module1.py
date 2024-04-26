4606e1d59caedc2bc7e94c8461462bc8a6bfb1fe
Wilk <s197653@student.pg.edu.pl>
2024-04-26 21:07:43 +0200

    pilokrztaltny i prostokatny sygnal względnie działają (bardzo względnie)

--- cmake_example_1/module1.py
+++ cmake_example_1/module1.py
@@ -1,10 +1,9 @@
 from build.Debug.cmake_example import *
 
-print (multiply(2,3))
-print (divide(2,3))
-
 print (sinus(1,10)) #czestotliwosc i czas probkowania
 print (cosinus(2,10))
 
-# print (prostakatny(0.5,10)) #czestotliwasc, czas probkowania i amplituda (domyslnie - 1)
-# print (prostakatny(0.5,10,2))
+
+print (pila(1,10,2))
+print (prostokatny(1,10,1)) #czestotliwasc, czas probkowania i amplituda
+print (prostokatny(0.5,10,2))
--- cmake_example_1/src/main.cpp
+++ cmake_example_1/src/main.cpp
@@ -29,20 +29,32 @@ std::vector<double> cosinus(double f, double time) //f - czestotliwosc
     mp::show();
     return y;
 }
-//std::vector<double> prostokatny(double f, double time, double max_value = 1) //f - czestotliwosc
-//{
-//    std::vector<double> x = mp::linspace(0, time, time * precision);
-//    std::vector<double> y = mp::transform(x, [&](auto x) { return ((x*2*max_value+1)2 - 1); });
-//    mp::plot(x, y);
-//    mp::xlabel("t_{s}");
-//    mp::ylabel("value");
-//    mp::show();
-//    return y;
-//}
+std::vector<double> pila(double f, double time, double max_value = 1) //f - czestotliwosc
+{
+    std::vector<double> x = mp::linspace(0, time, time * precision);
+    std::vector<double> y = mp::transform(x, [&](auto x) { return max_value*(x - floor(x)); });
+    mp::plot(x, y);
+    mp::xlabel("t_{s}");
+    mp::ylabel("value");
+    mp::show();
+    return y;
+}
+
+std::vector<double> prostokatny(double f, double time, double max_value = 1) //f - czestotliwosc
+{
+    std::vector<double> x = mp::linspace(0, time, time * precision);
+    std::vector<double> y = mp::transform(x, [&](auto x) { return ((x - floor(x)) > 0 ? max_value : 0 - max_value); });
+    mp::plot(x, y);
+    mp::xlabel("t_{s}");
+    mp::ylabel("value");
+    mp::show();
+    return y;
+}
 
 
 PYBIND11_MODULE(cmake_example, m) {
     m.def("sinus", &sinus);
     m.def("cosinus", &cosinus);
-   // m.def("prostokatny", &prostokatny);
+    m.def("prostokatny", &prostokatny);
+    m.def("pila", &pila);
 }
