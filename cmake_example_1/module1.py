ce2239fffab3fb93ff0990e674f76eb5d26c4845
Wilk <s197653@student.pg.edu.pl>
2024-04-28 16:05:22 +0200

    filtracja wstęp!!! convolution

--- cmake_example_1/module1.py
+++ cmake_example_1/module1.py
@@ -1,9 +1,11 @@
 from build.Debug.cmake_example import *
 
 # print (sinus(1,10)) #czestotliwosc i czas probkowania w sekundach
-# print (cosinus(2,10))
+# print (cosinus(1,10))
+
+print (convolution(sinus(1,10),cosinus(1,10)))
 
 
 # print (pila(1,4,2))
-print (prostokatny(1,4,1)) #czestotliwasc, czas probkowania w sekundach i amplituda
-print (prostokatny(0.5,4,2))
+# print (prostokatny(1,4,1)) #czestotliwasc, czas probkowania w sekundach i amplituda
+# print (prostokatny(0.5,4,2))
--- cmake_example_1/src/main.cpp
+++ cmake_example_1/src/main.cpp
@@ -51,10 +51,33 @@ std::vector<double> prostokatny(double f, double time, double max_value = 1) //f
     return y;
 }
 
+std::vector<double> convolution(std::vector<double>& funkcja, std::vector<double>& kernel)
+{
+    std::vector<double> Wygladzony(funkcja.size());
+    double time = funkcja.size();
+
+    for (int i = 0; i < funkcja.size(); ++i)
+    {
+        double sum = 0.0;
+        for (int j = kernel.size(); j > 0; --j)
+        {
+            if (i >= j) sum += funkcja[i - j] * kernel[j];
+        }
+        Wygladzony[i] = sum;
+    }
+
+    std::vector<double> x = mp::linspace(0, time, time * f_probkowania);
+
+    mp::plot(x, Wygladzony);
+    mp::show();
+    return Wygladzony;
+}
+
 
 PYBIND11_MODULE(cmake_example, m) {
     m.def("sinus", &sinus);
     m.def("cosinus", &cosinus);
     m.def("prostokatny", &prostokatny);
     m.def("pila", &pila);
+    m.def("convolution", &convolution);
 }
