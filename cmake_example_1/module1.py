4d913cf19d8daffe45bee29d1038a562641b4721
Wilk <s197653@student.pg.edu.pl>
2024-04-28 15:11:06 +0200

    generowanie sygnalow cos, sin, pila, prostokat działaja

--- cmake_example_1/module1.py
+++ cmake_example_1/module1.py
@@ -1,9 +1,9 @@
 from build.Debug.cmake_example import *
 
-print (sinus(1,10)) #czestotliwosc i czas probkowania
+print (sinus(1,10)) #czestotliwosc i czas probkowania w sekundach
 print (cosinus(2,10))
 
 
 print (pila(1,10,2))
-print (prostokatny(1,10,1)) #czestotliwasc, czas probkowania i amplituda
+print (prostokatny(1,10,1)) #czestotliwasc, czas probkowania w sekundach i amplituda
 print (prostokatny(0.5,10,2))
