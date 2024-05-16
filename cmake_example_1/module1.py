8c229b72a5c5f6247c98fdf3d18d5f7c095f655f
Wilk <s197653@student.pg.edu.pl>
2024-05-16 18:01:11 +0200

    wszystko oprocz filtracji 2d

--- cmake_example_1/module1.py
+++ cmake_example_1/module1.py
@@ -1,11 +1,16 @@
 from build.Debug.cmake_example import *
 
-# print (sinus(1,10)) #czestotliwosc i czas probkowania w sekundach
+#print (sinus(1,10)) #czestotliwosc i czas probkowania w sekundach
 # print (cosinus(1,10))
 
-print (convolution(sinus(1,100),cosinus(1,100)))
+#print (krzywa_gaussa(1,0,10)) #sigma, mediana, czas probkowania w sekundach
+
+#print(wzmocnienie_dzwieku("cmake_example_1/dc.wav",2,"cmake_example_1/wzmocnionedc.wav"))
+
+print(wykres_dzwieku("cmake_example_1/dc.wav"))
+# print (korelacja(wykres_dzwieku("cmake_example_1/dc.wav"),sinus(0.2,2000)))
 
 
-print (pila(1,4,2))
-print (prostokatny(1,4,1)) #czestotliwasc, czas probkowania w sekundach i amplituda
+# print (pila(1,4,2))
+#print (prostokatny(1,4,1)) #czestotliwasc, czas probkowania w sekundach i amplituda
 # print (prostokatny(0.5,4,2))
--- cmake_example_1/src/main.cpp
+++ cmake_example_1/src/main.cpp
@@ -3,17 +3,19 @@
 #include <vector>
 #include <matplot/matplot.h>
 #include <cmath>
+#include <cstring>
 #include <AudioFile.h>
 
 namespace mp = matplot;
 namespace py = pybind11;
 
 const int f_probkowania = 48;
+const double PI = 3.14159265359;
 
 std::vector<double> sinus(double f, double time) //f - czestotliwosc
 {
     std::vector<double> x = mp::linspace(0, time, time*f_probkowania);
-    std::vector<double> y = mp::transform(x, [&](auto x) { return sin(f*x); });
+    std::vector<double> y = mp::transform(x, [&](auto x) { return sin(f*x*2*PI); });
     mp::plot(x, y);
     mp::xlabel("t_{s}");
     mp::ylabel("value");
@@ -23,7 +25,7 @@ std::vector<double> sinus(double f, double time) //f - czestotliwosc
 std::vector<double> cosinus(double f, double time) //f - czestotliwosc
 {
     std::vector<double> x = mp::linspace(0, time, time*f_probkowania);
-    std::vector<double> y = mp::transform(x, [&](auto x) { return cos(f*x); });
+    std::vector<double> y = mp::transform(x, [&](auto x) { return cos(f*x*2*PI); });
     mp::plot(x, y);
     mp::xlabel("t_{s}");
     mp::ylabel("value");
@@ -74,6 +76,111 @@ std::vector<double> convolution(std::vector<double>& funkcja, std::vector<double
     return Wygladzony;
 }
 
+std::vector<double> krzywa_gaussa(double sigma, double m, int time) //m to mediana
+{
+    std::vector<double> x = mp::linspace(0, time, time * f_probkowania);
+    std::vector<double> kernel = mp::transform(x, [&](auto x) { return ((1 / (sigma * sqrt(2 * PI))) * exp(-pow((x-m), 2) / (2 * sigma * sigma))); });
+    mp::plot(x, kernel);
+    mp::show();
+   
+    return kernel;
+}
+
+std::vector<double> wzmocnienie_dzwieku(std::string nazwa, double k, std::string zapis)
+{
+    AudioFile<double> audioFile;
+    audioFile.load(nazwa);
+
+    std::vector<double> y;
+    int channel = 0;
+    int n = audioFile.getNumSamplesPerChannel();
+
+    for (int i = 0; i < n; i++)
+    {
+        audioFile.samples[channel][i] *= k;
+        y.push_back(audioFile.samples[channel][i]);
+    }
+
+    audioFile.save(zapis);
+
+    mp::plot(y);
+    mp::show();
+
+    return y;
+
+}
+
+std::vector<double> wzmocnienie(std::vector<double>& funkcja, double k)
+{
+    for (int i = 0; i < funkcja.size(); i++) funkcja[i] *= k;
+
+    mp::plot(funkcja);
+    mp::show();
+
+    return funkcja;
+}
+
+std::vector<double> autokorelacja(std::vector<double>& funkcja)
+{
+    std::vector<double> funkcja2 = funkcja;
+
+    std::vector<double> skorelowany(funkcja.size()+funkcja2.size()-1);
+
+    for (int i = 0; i < funkcja.size(); ++i)
+    {
+        for (int j = funkcja2.size()-1; j > 0; --j)
+        {
+            skorelowany[i+(funkcja2.size()-j-1)] += funkcja[i] * funkcja2[j];
+        }
+    }
+
+    std::vector<double> x = mp::linspace(0, skorelowany.size(), skorelowany.size() * f_probkowania);
+
+    mp::plot(x, skorelowany);
+    mp::show();
+
+    return skorelowany;
+}
+
+std::vector<double> korelacja(std::vector<double>& funkcja, std::vector<double>& funkcja2)
+{
+    std::vector<double> skorelowany(funkcja.size() + funkcja2.size() - 1);
+
+    for (int i = 0; i < funkcja.size(); ++i)
+    {
+        for (int j = funkcja2.size() - 1; j > 0; --j)
+        {
+            skorelowany[i + (funkcja2.size() - j - 1)] += funkcja[i] * funkcja2[j];
+        }
+    }
+
+    std::vector<double> x = mp::linspace(0, skorelowany.size(), skorelowany.size() * f_probkowania);
+
+    mp::plot(x, skorelowany);
+    mp::show();
+
+    return skorelowany;
+}
+
+std::vector<double> wykres_dzwieku(const std::string nazwa)
+{
+    AudioFile<double> audioFile;
+    audioFile.load(nazwa);
+    audioFile.printSummary();
+
+    std::vector<double> y;
+
+    for (const auto& sampel : audioFile.samples[0])
+    {
+        double sample = sampel;
+        y.push_back(sample);
+    }
+
+    mp::plot(y);
+    mp::show();
+
+    return y;
+}
 
 PYBIND11_MODULE(cmake_example, m) {
     m.def("sinus", &sinus);
@@ -81,4 +188,10 @@ PYBIND11_MODULE(cmake_example, m) {
     m.def("prostokatny", &prostokatny);
     m.def("pila", &pila);
     m.def("convolution", &convolution);
+    m.def("krzywa_gaussa", &krzywa_gaussa);
+    m.def("wzmocnienie", &wzmocnienie);
+    m.def("wzmocnienie_dzwieku", &wzmocnienie_dzwieku);
+    m.def("autokorelacja", &autokorelacja);
+    m.def("korelacja", &korelacja);
+    m.def("wykres_dzwieku", &wykres_dzwieku);
 }
